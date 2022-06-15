dd7c78e64837e4e186e09270ead1f887514c1b0c
Robert Di Ninno <robertdininno@Roberts-MacBook-Pro.local>
2022-06-15 10:41:55 -0700

    adjust props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -9,7 +9,7 @@ export default function interviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === value}
+        selected={interviewer.id === props.value} 
         setInterviewer={() => props.onChange(interviewer.id)}
       />
     )
