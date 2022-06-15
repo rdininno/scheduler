7bb87bdeb4e5f3fc1c03ca12014f27b4e782ad69
Robert Di Ninno <robertdininno@Roberts-MacBook-Pro.local>
2022-06-14 18:43:02 -0700

    refactor

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+  const dayItems = props.days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.value}
+        setDay={props.onChange}
+      />
+    )
+  })
+
+  return (
+    <ul>
+      {dayItems}
+    </ul>
+  );
+}
