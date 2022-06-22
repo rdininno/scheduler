dcdb488040e8053dd1ce5b9103ff2056e8589632
Robert Di Ninno <robertdininno@Roberts-MacBook-Pro.local>
2022-06-22 12:02:13 -0700

    add attr for testing

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss";
+import PropTypes from 'prop-types';
 
-export default function interviewerList(props) {
+export default function InterviewerList(props) {
   const interviewersArray = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
@@ -25,3 +26,7 @@ export default function interviewerList(props) {
 
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
